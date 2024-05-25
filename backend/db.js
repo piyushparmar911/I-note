d04bf8fa2e0a02837d8fc1709613fb7e90d62b79
piyushparmar911 <prparmar911@gmail.com>
2024-05-25 12:45:44 +0530

    express validator added

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/i-note";  // Replace 'yourDatabaseName' with the actual name of your MongoDB database.
+const mongoURI = "mongodb://localhost:27017/i-notes";  // Replace 'yourDatabaseName' with the actual name of your MongoDB database.
 
 const connectToMongoo =  () => {
         // Connect to MongoDB without deprecated options
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../modules/User')
+const User = require('../modules/User');
+const { body, validationResult } = require('express-validator'); 
 
-router.post ('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-
-})
+router.post('/' ,[
+    body('name', 'name is not valid').isLength({min: 3}),
+    body('email', 'email is not valid').isEmail(),
+    body('password', 'not valid password').isLength({min: 6})
     
+],(req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      }).then(user => res.json(user))
+      .catch(err => {console.log(err)
+      res.json({err: "enter unique email"})});
+})
+
 
 module.exports = router;
