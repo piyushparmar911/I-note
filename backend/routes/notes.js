e67492fa5c5af701d7a3abaf309a35718561bab1
piyushparmar911 <prparmar911@gmail.com>
2024-05-28 19:58:01 +0530

    note update operation created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -46,4 +46,34 @@ router.post(
     }
   }
 );
+
+// route 3: update  notes using : put "api/notes/updatenote/:id" login required
+router.put(
+  "/updatenote/:id",
+  fetchuser,  async (req, res) => {
+   const {title, description,tag} = req.body;
+  //  create a new note object
+  const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.despriction = description};
+    if(tag){newNote.tag = tag};
+
+
+    const note = await Notes.findById(req.params.id);
+    if(!note)
+      {
+        return res.status(404).json({error: "note not found"});
+      }
+      if(note.user.toString() !== req.user.id)
+        {
+          return res.status(401).json({error: "not allowed"});
+        }
+
+     await Notes.findByIdAndUpdate(req.params.id ,{$set: newNote} , {new: true});
+        res.json(note);
+  }
+);
+
+
+
 module.exports = router;
