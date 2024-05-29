6af8e379ba65e18013c2662ad2045db3724c709a
piyushparmar911 <prparmar911@gmail.com>
2024-05-29 10:05:32 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <h1 >this is i-note app</h1>      
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
